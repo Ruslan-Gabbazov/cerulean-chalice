26e30a963ec0b79bcd05001fc9ea227659564809
Ruslan-Gabbazov <58855790+Ruslan-Gabbazov@users.noreply.github.com>
2023-02-27 02:27:51 +0300

    delete redundant route

--- app/chat/routes.py
+++ app/chat/routes.py
@@ -1,6 +1,4 @@
-import os
 import typing
-from app import BASE_DIR
 
 if typing.TYPE_CHECKING:
     from app.chat.app import Application
@@ -10,6 +8,5 @@ def setup_routes(app: 'Application'):
     from app.chat.views import WSConnectView, IndexView, ChatView
 
     app.router.add_view('/connect', WSConnectView)
-    app.router.add_static("/static", os.path.join(BASE_DIR, "client", "static"))
     app.router.add_view("/", IndexView)
     app.router.add_view("/chat", ChatView)
